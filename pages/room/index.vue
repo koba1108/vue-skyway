1a10217a721c8759bbebbc5e24635f46c42db8a0
koba1108 <paseli.koba@gmail.com>
2020-04-24 20:56:18 +0900

    add

--- pages/room/index.vue
+++ pages/room/index.vue
@@ -1,13 +1,52 @@
 <template>
   <div>
-    {{ memberStreams }}
-    <video id="my-video" autoplay playsinline/>
-    <video id="other-video" autoplay playsinline/>
-    <video id="other2-video" autoplay playsinline/>
-    <label>
-      <input style="border: solid #000000" v-model="roomName">
-    </label>
-    <v-btn :disabled="!roomName" @click="newRoom">newRoom</v-btn>
+    <template v-if="isRoomJoined && memberStreams.length < 2">
+      <div class="container" v-if="memberStreams.length > 0">
+        <h4>メンバーID</h4>
+        <ul class="member-list">
+          <li v-for="m in memberStreams" :key="m.peerId">{{ m.peerId }}</li>
+        </ul>
+      </div>
+      <div class="container" v-else>
+        <h4>まだ誰もいません</h4>
+      </div>
+    </template>
+
+    <div v-show="isRoomJoined" class="my-video-container" @click="changeViewMode">
+      <video id="my-video" ref="myVideo" :srcObject.prop="stream" autoplay playsinline/>
+    </div>
+
+    <div class="video-member-container">
+      <div class="video-box" v-for="m in memberStreams" :key="m.peerId">
+        <!--div class="user-label">{{ m.peerId }}</div-->
+        <video :id="m.peerId" :srcObject.prop="m.stream" autoplay playsinline/>
+      </div>
+    </div>
+
+    <div class="footer container">
+      <template v-if="isMenuShow">
+        <div>
+          <label>
+            ルームID：
+            <input type="text" v-model="roomId" :disabled="isRoomJoined">
+          </label>
+        </div>
+        <div>
+          <label>
+            ニックネーム：
+            <input type="text" v-model="nickname" :disabled="isRoomJoined">
+          </label>
+        </div>
+        <div>
+          <v-btn v-if="isRoomJoined" @click="newRoom">退室する</v-btn>
+          <v-btn v-else :disabled="!roomId || !nickname" @click="newRoom">入室する</v-btn>
+          <v-btn @click="isMenuShow = false">メニューを閉じる</v-btn>
+        </div>
+      </template>
+      <template v-else>
+        <v-btn @click="isMenuShow = true">メニューを開く</v-btn>
+      </template>
+    </div>
   </div>
 </template>
 
@@ -35,23 +74,30 @@
     peer: Peer = new Peer({
       key: this.skywayApiKey,
     })
-    stream: MediaStream | undefined
+    stream: MediaStream | null = null
+    isRoomJoined: boolean = false
+    isMenuShow: boolean = true
     room: any
-    roomName: string = ''
+    roomId: string = 'koba'
+    nickname: string = 'koba'
     memberStreams: Array<MemberStream> = []
 
     newRoom() {
-      this.room = this.peer.joinRoom(this.roomName, {
-        mode: MODE_SFU,
-        stream: this.stream,
-      })
-      this.room.on('open', this.onRoomOpen)
-      this.room.on('peerJoin', this.onPeerJoin)
-      this.room.on('peerLeave', this.onPeerLeave)
-      this.room.on('stream', this.onStream)
-      this.room.on('log', this.onLog)
-      this.room.on('data', this.onData)
-      this.room.on('close', this.onClose)
+      if (this.stream) {
+        this.room = this.peer.joinRoom(this.roomId, {
+          mode: MODE_SFU,
+          stream: this.stream,
+        })
+        this.room.on('open', this.onRoomOpen)
+        this.room.on('peerJoin', this.onPeerJoin)
+        this.room.on('peerLeave', this.onPeerLeave)
+        this.room.on('stream', this.onStream)
+        this.room.on('log', this.onLog)
+        this.room.on('data', this.onData)
+        this.room.on('close', this.onClose)
+      } else {
+        alert('カメラとマイクを許可してください')
+      }
     }
 
     onStream(stream: RoomStream) {
@@ -60,39 +106,26 @@
       if (!memberStream) {
         this.memberStreams.push({
           peerId: stream.peerId,
+          stream: <MediaStream>stream
         })
       }
-      if (this.memberStreams.length == 2) {
-        const other2Video: HTMLVideoElement = <HTMLVideoElement>document.getElementById('other2-video')
-        if (other2Video) {
-          other2Video.srcObject = stream
-        }
-      } else if (this.memberStreams.length == 1) {
-        const otherVideo: HTMLVideoElement = <HTMLVideoElement>document.getElementById('other-video')
-        if (otherVideo) {
-          otherVideo.srcObject = stream
-        }
+      if (this.memberStreams.length > 1) {
+        this.isMenuShow = false
       }
     }
 
     onRoomOpen() {
       console.log('onRoomOpen')
-      const myVideo: HTMLVideoElement = <HTMLVideoElement>document.getElementById('my-video')
-      if (myVideo) {
-        myVideo.srcObject = <MediaStream>this.stream
-      }
+      this.isRoomJoined = true
     }
 
     onPeerJoin(peerId: string) {
       console.log('onPeerJoin peerId', peerId)
-      this.memberStreams.push({
-        peerId: peerId,
-      })
     }
 
     onPeerLeave(peerId: string) {
       console.log('onPeerLeave peerId', peerId)
-      this.memberStreams = this.memberStreams.filter(member => member.peerId === peerId)
+      this.memberStreams = this.memberStreams.filter(member => member.peerId !== peerId)
     }
 
     onLog(logs: Array<string>) {
@@ -107,6 +140,30 @@
       console.log('onClose')
     }
 
+    changeViewMode() {
+      console.log('changeViewMode')
+    }
+
+    public get myVideoStyle(): Object {
+      console.log('this.$refs', this.$refs)
+      if (this.$ref !== {} && this.$refs.myVideo) {
+        const videoHeight = this.$refs.myVideo.videoHeight
+        const videoWidth = this.$refs.myVideo.videoWidth
+        return videoHeight > videoWidth ? {
+          height: `${videoHeight}px`,
+          width: `${videoHeight}px`,
+        } : {
+          height: `${videoWidth}px`,
+          width: `${videoWidth}px`,
+        }
+      } else {
+        return {
+          height: '100vh',
+          width: '100vw',
+        }
+      }
+    }
+
     async mounted() {
       this.stream = await navigator.mediaDevices.getUserMedia({
         audio: false,
@@ -119,5 +176,70 @@
 <style lang="scss" scoped>
   video {
     transform: scale(-1, 1);
+  }
+
+  input[type=text] {
+    border: solid 1px #000000;
+    padding-left: 5px;
+  }
+
+  .container {
+    padding: 10px;
+
+    div {
+      padding-top: 5px;
+    }
+  }
+
+  .my-video-container {
+    position: fixed;
+    right: 10px;
+    bottom: 10px;
+    z-index: 10;
+
+    #my-video {
+      background-color: black;
+      border-radius: 50%;
+      border: solid 5px red;
+    }
+  }
+
+  .video-member-container {
+    position: fixed;
+    height: 100vh;
+    width: 100vw;
+    display: grid;
+    grid-row: 1 / 4;
+    grid-column: 1 / 4;
+
+    .video-box {
+      position: relative;
+      top: 0;
+      max-height: 40vh;
+
+      .user-label {
+        position: relative;
+        top: 35px;
+        left: 5px;
+        height: 24px;
+        width: 200px;
+        text-align: center;
+        color: white;
+        background-color: black;
+        border-radius: 20px;
+        z-index: 5;
+      }
+
+      video {
+        max-height: 40vh;
+        max-width: 100%;
+        height: auto;
+      }
+    }
+  }
+
+  .footer {
+    position: fixed;
+    bottom: 10px;
   }
 </style>
